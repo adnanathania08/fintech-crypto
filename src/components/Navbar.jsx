f23d969304a545be1b652ea802be461a5f15073f
Adnan Athania <adnanathania9@gmail.com>
2025-01-23 16:06:02 +0530

    changes done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       <Disclosure as="nav" className="">
         <div className="mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-20 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
               {/* Mobile menu button*/}
               <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                 <span className="absolute -inset-0.5" />
@@ -36,11 +36,11 @@ const Navbar = () => {
                 />
               </DisclosureButton>
             </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-1 items-center justify-center sm:items-stretch lg:justify-start">
               <div className="flex shrink-0 items-center font-quicksand">
                 <GrTechnology className="h-10 w-auto text-txtwhite" />
               </div>
-              <div className="hidden sm:ml-6 sm:flex items-center">
+              <div className="hidden sm:ml-6 lg:flex items-center">
                 <div className="flex space-x-8 items-center">
                   <p className="text-white">Prices</p>
                   <p className="text-white">Products</p>
@@ -49,7 +49,7 @@ const Navbar = () => {
                 </div>
               </div>
             </div>
-            <div className="absolute inset-y-0 right-0  items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 sm:flex sm:space-x-4 hidden ">
+            <div className="absolute inset-y-0 right-0  items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 lg:flex sm:space-x-4 hidden ">
               <button className="font-quicksand font-bold border-2 border-pgreen py-2 px-4 text-pgreen rounded-full">
                 Start Now
               </button>
@@ -62,7 +62,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        <DisclosurePanel className="sm:hidden absolute z-50 bg-black w-full">
+        <DisclosurePanel className="lg:hidden absolute z-50 bg-black w-full">
           <div className="space-y-1 flex flex-col justify-between px-2 pb-3 pt-2 h-[60vh] shadow-sm shadow-gray-700 rounded-b-xl">
             <div className="space-y-6 border-b border-gray-600 pb-6">
               <p className="text-white">Prices</p>
